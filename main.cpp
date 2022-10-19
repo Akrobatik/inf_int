db89fa5ae0c28ad7248fbd5b0357f48b2cc48090
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2022-10-19 09:33:48 +0900

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,36 +1,59 @@
-#include <stdlib.h>
-#include <string.h>
-
-#include <iostream>
-
 #include "inf_int.h"
 
-using namespace std;
-
 int main() {
-  inf_int a, e;
+  inf_int a;
+  std::cout << "a : " << a << std::endl;
+
   inf_int b(100);
-  inf_int c("321111111111122222222222233333333333444444444445555555555");
-  inf_int d("123451987651234572749499923455022211");
-  inf_int f = d;
-  inf_int g(f);
+  std::cout << "b : " << b << std::endl;
 
-  // cin >> g ;   // not required
+  inf_int c("-123456");
+  std::cout << "c : " << c << std::endl;
 
-  a = b * c;
-  // e=g/f;       // not required
+  inf_int d(c);
+  std::cout << "d : " << d << std::endl;
 
-  b = c - d;
+  inf_int e = c;
+  std::cout << "e : " << e << std::endl;
 
-  if (f == d) {
-    cout << "a : " << a << endl;
-    cout << "b : " << b << endl;
-    cout << "c : " << c << endl;
-    cout << "d : " << d << endl;
-    // cout << "e : " << e << endl;
-    cout << "f : " << f << endl;
-    // cout << "g : " << g << endl;
-  }
+  inf_int f = inf_int(10000) + inf_int(20000);
+  std::cout << "f : " << f << std::endl;
+
+  inf_int g = inf_int(10000) - inf_int(20000);
+  std::cout << "g : " << g << std::endl;
+
+  inf_int h = inf_int("10000000000") * inf_int("123456789123456789");
+  std::cout << "h : " << h << std::endl;
+
+  std::cout << "b == e : " << std::boolalpha << (b == e) << std::endl;
+  std::cout << "c == e : " << std::boolalpha << (c == e) << std::endl;
+
+  std::cout << "b != e : " << std::boolalpha << (b != e) << std::endl;
+  std::cout << "c != e : " << std::boolalpha << (c != e) << std::endl;
+
+  std::cout << "b > e : " << std::boolalpha << (b > e) << std::endl;
+  std::cout << "c > e : " << std::boolalpha << (c > e) << std::endl;
+
+  std::cout << "e < b : " << std::boolalpha << (e < b) << std::endl;
+  std::cout << "e < c : " << std::boolalpha << (e < c) << std::endl;
+
+  inf_int* org = new inf_int(100);
+  inf_int copy1 = *org;
+  inf_int copy2(*org);
+
+  delete org;
+
+  [&]() -> void {
+    __try {
+      std::cout << "copy1 : " << copy1 << std::endl;
+      std::cout << "operator= -> Deep Copy" << std::endl;
+
+      std::cout << "copy2 : " << copy2 << std::endl;
+      std::cout << "copy constructor -> Deep Copy" << std::endl;
+    } __except (1) {
+      std::cout << "Exception by Shallow Copy" << std::endl;
+    }
+  }();
 
   return 0;
 }
